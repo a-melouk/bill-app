6563dca2374485808a2a17fe66f21437b23de87f
mabdou <abdoumelouk96@gmail.com>
2024-01-08 15:47:43 +0100

    Updated test for checking file upload

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -9,24 +9,25 @@ import NewBill from '../containers/NewBill.js'
 import NewBillUI from '../views/NewBillUI.js'
 
 describe('Given I am connected as an employee', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname })
+    }
+    document.body.innerHTML = NewBillUI()
+
+    const newBill = new NewBill({
+      document,
+      onNavigate,
+      store: null,
+      localStorage: window.localStorage,
+    })
+  })
+
   describe('When I download a file with an incorrect extension', () => {
     test('Then an alert should be displayed', () => {
       const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {})
-
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
-
-      document.body.innerHTML = NewBillUI()
-
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: null,
-        localStorage: window.localStorage,
-      })
 
       const inputFile = screen.getByTestId('file')
       fireEvent.change(inputFile, {
